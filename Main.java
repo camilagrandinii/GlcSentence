7a30f707885dbba55591df11a87096ce3d1e6867
Camila Lacerda Grandini <cacagrandini@gmail.com>
2023-11-12 20:40:57 -0300

    Added logic to test our grammar detector

--- Main.java
+++ Main.java
@@ -13,36 +13,28 @@ class Main {
     public static void main(String[] args) throws IOException {
         GrammarExtractor grammarExtractor = new GrammarExtractor();
         GrammarConversor grammarConversor = new GrammarConversor();
+        CYK cykExecutor = new CYK();
 
-        String nome_arquivo = "gramatica.txt"; // Substitua pelo nome do arquivo que deseja ler
+        String nome_arquivo = "gramatica3.txt"; // Substitua pelo nome do arquivo que deseja ler
         BufferedReader bf = new BufferedReader(new FileReader(nome_arquivo));
 
         Grammar grammar = new Grammar();
 
         String linha = bf.readLine();
         grammar = grammarExtractor.ExtractGrammar(linha, grammar);
+        
+        grammar.PrintGrammar();
+        
+        grammar = grammarConversor.ToFncGrammar(grammar);
 
         while ((linha = bf.readLine()) != null) {
-            // Adicionar tratativas para os testes
+            //boolean cykResult = cykExecutor.cyk(ruleArrayGrammar.rules, linha);
+
+            //System.out.println("A string '" + linha + "' " + (cykResult ? "faz" : "não faz") + " parte da gramática.");
         }
 
         bf.close();
-
-        grammar.PrintGrammar();
-        grammarConversor.ToFncGrammar(grammar);
-        // String sentenceToCheck = "a";
-
-        // // Chame o método para verificar se a sentença pertence à linguagem
-        // boolean belongsToLanguage = grammarConversor.checkSentenceBelongsLanguage(grammar, sentenceToCheck);
-
-        // // Exiba o resultado
-        // if (belongsToLanguage) {
-        //     System.out.println("A sentença pertence à linguagem.");
-        // } else {
-        //     System.out.println("A sentença não pertence à linguagem.");
-        // }
     }
 }
 // se passa so vazio ta quebrando 
-// nao fizemos o tratamento de bb duas regras lower case seguidas uma da outra
 // nao fizemos tambem dois seguidos tipo aBB b-> lambda ele identifica aB duas vezes
